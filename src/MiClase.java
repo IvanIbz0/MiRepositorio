cfa5bc4819c5d1d5623d7f42a43afd936121526b
Iván <iideopacuasanchez@gmail.com>
2021-02-04 16:53:39 +0100

    Versión Inicial, no hay nada.
    Signed-off-by: Iván<iideopacuasanchez@gmail.com>

---
+++ src/MiClase.java
@@ -0,0 +1,13 @@
+
+public class MiClase {
+	public static void main(String[] args)
+	{
+		System.out.println("esto es una prueba");
+		otroMetodo();
+	}
+	
+	private static void otroMetodo()
+	{
+		System.out.println("Otro metodo");
+	}
+}
